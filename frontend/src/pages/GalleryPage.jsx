705ec63ab0bbfb69725a8e3e510d3ca8cc649cef
VeerDoria <veerdoria62@gmail.com>
2025-06-29 13:48:21 +0530

    add images to gallery

--- frontend/src/pages/GalleryPage.jsx
+++ frontend/src/pages/GalleryPage.jsx
@@ -1,14 +1,24 @@
-import idm1 from '../assets/images/IDM/eye1.jpg'
-import idm2 from '../assets/images/IDM/eye2.jpg'
-import idm3 from '../assets/images/IDM/eye3.jpg'
-import idm4 from '../assets/images/IDM/eye4.jpg'
-import idm5 from '../assets/images/IDM/eye5.jpg'
-import teachersDay1 from '../assets/images/teachers-day/eye1.jpg'
-import teachersDay2 from '../assets/images/teachers-day/eye2.jpg'
-import teachersDay3 from '../assets/images/teachers-day/eye3.jpg'
-import teachersDay4 from '../assets/images/teachers-day/eye4.jpg'
-import teachersDay5 from '../assets/images/teachers-day/eye5.jpg'
-import teachersDay6 from '../assets/images/teachers-day/eye6.jpg'
+import idm1 from '../assets/images/IDM/001.jpg'
+import idm2 from '../assets/images/IDM/002.jpg'
+import idm3 from '../assets/images/IDM/003.jpg'
+import idm4 from '../assets/images/IDM/004.jpg'
+import idm5 from '../assets/images/IDM/005.jpg'
+import teachersDay1 from '../assets/images/teachers-day/001.jpg'
+import teachersDay2 from '../assets/images/teachers-day/002.jpg'
+import teachersDay3 from '../assets/images/teachers-day/003.jpg'
+import teachersDay4 from '../assets/images/teachers-day/004.jpg'
+import teachersDay5 from '../assets/images/teachers-day/005.jpg'
+import freshersParty1 from '../assets/images/freshers-party/001.jpg'
+import freshersParty2 from '../assets/images/freshers-party/002.jpg'
+import freshersParty3 from '../assets/images/freshers-party/003.jpg'
+import freshersParty4 from '../assets/images/freshers-party/004.jpg'
+import freshersParty5 from '../assets/images/freshers-party/005.jpg'
+import mathsQuest1 from '../assets/images/maths-quest/001.jpg'
+import mathsQuest2 from '../assets/images/maths-quest/002.jpg'
+import mathsQuest3 from '../assets/images/maths-quest/003.jpg'
+import mathsQuest4 from '../assets/images/maths-quest/004.jpg'
+import mathsQuest5 from '../assets/images/maths-quest/005.jpg'
+
 import ImagesContainer from '../components/ImagesContainer'
 
 const GalleryPage = () => {
@@ -20,19 +30,19 @@ const GalleryPage = () => {
         'The International Day of Mathematics celebrates the beauty, creativity, and universal language of mathematics. Through engaging exhibitions, puzzles, talks, and interactive sessions, the event highlights how math shapes our world — from daily life to groundbreaking discoveries. It serves as a platform to inspire curiosity and foster appreciation for mathematical thinking.',
     },
     {
-      imageUrls: [teachersDay1,teachersDay2,teachersDay3,teachersDay4,teachersDay5,teachersDay6],
+      imageUrls: [teachersDay1,teachersDay2,teachersDay3,teachersDay4,teachersDay5],
       title: "Teacher's Day Celebration",
       description:
         `A heartfelt tribute to our mentors and educators, Teacher's Day is a celebration of their unwavering guidance, patience, and dedication. The event features cultural performances, student-led activities, and special gestures of appreciation to honor the pillars of learning.`,
     },
     {
-      imageUrls: [idm1, teachersDay1],
-      title: 'Olympiad',
+      imageUrls: [mathsQuest1,mathsQuest2,mathsQuest3,mathsQuest4,mathsQuest5],
+      title: 'Math Quest',
       description:
-        'The Olympiad is a platform for academic brilliance, where students challenge themselves across various subjects and problem-solving domains. The event encourages critical thinking, competitive spirit, and intellectual excellence in a structured, rewarding environment.',
+        'Description About maths quest',
     },
     {
-      imageUrls: [idm2, teachersDay2],
+      imageUrls: [freshersParty1, freshersParty2,freshersParty3,freshersParty4,freshersParty5],
       title: "Fresher's Welcome Party",
       description:
         `A vibrant and lively welcome event for the incoming batch, the Fresher's Party sets the tone for an exciting journey ahead. With energetic performances, interactive games, and a warm sense of belonging, it's an unforgettable introduction to campus life.`,
